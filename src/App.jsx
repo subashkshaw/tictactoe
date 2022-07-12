1c9eba24ff55c6de29c9e920114dc265cbf42ae9
subashkshaw <subash.k.shaw@gmail.com>
2022-07-12 11:21:52 +0530

    displayed tie status

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './styles/root.scss';
 import Board from './components/Board';
 import History from './components/History';
 import { calculateWinner } from './helper';
+import StatusMessage from './components/StatusMessage';
 
 const App = () => {
   // const [board, setBoard] = useState(Array(9).fill(null));
@@ -13,9 +14,6 @@ const App = () => {
   const [currentMov, setCurrentMov] = useState(0);
   const current = history[currentMov];
   const winner = calculateWinner(current.board);
-  const message = winner
-    ? `Winner is ${winner}`
-    : `Next Player is ${current.isXNext ? 'X' : 'O'}`;
   const handleSquareClick = position => {
     if (current.board[position] || winner) {
       return;
@@ -41,7 +39,7 @@ const App = () => {
     <>
       <div className="app">
         <h1>TIC TAC TOE</h1>
-        <h2>{message}</h2>
+        <StatusMessage winner={winner} current={current} />
         <Board board={current.board} handleSquareClick={handleSquareClick} />
         <History history={history} moveTo={moveTo} currentMov={currentMov} />
       </div>
